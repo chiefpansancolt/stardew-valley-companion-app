707121701dc2c7a8924cd0ee02c64c8a0d073cfb
Christopher Pezza <chris.pezza@hey.com>
2021-01-05 16:58:38 -0500

    WIP

--- frontend/javascript/constants.js
+++ frontend/javascript/constants.js
@@ -3,6 +3,7 @@ const CONSTANTS = {
   BLANKSPACE: ' ',
   BLANK: '',
   PLAYTHROUGHS: 'playthroughs',
+  UTF8: 'UTF-8',
   TARGETS: {
     CONTENT: 'content',
     MENU: 'menu',
@@ -14,6 +15,12 @@ const CONSTANTS = {
     ICONOFF: 'iconOff',
     BACKGROUND: 'background',
     VIEW: 'view',
+    TEMPLATE: 'template',
+    GALLERY: 'gallery',
+    EMPTY: 'empty',
+    INPUT: 'input',
+    TEXTAREA: 'textarea',
+    DROPZONE: 'dropzone',
   },
   CLASSES: {
     HIDE: 'hide',
@@ -25,6 +32,19 @@ const CONSTANTS = {
     FROMBACKGROUND: 'fromBackground',
     TOVIEW: 'toView',
     FROMVIEW: 'fromView',
+  },
+  DOM: {
+    H1: 'h1',
+    LI: 'li',
+  },
+  CLASS: {
+    DELETE: '.delete',
+    SIZE: '.size'
+  },
+  TYPE: {
+    MB: 'mb',
+    KB: 'kb',
+    BYTE: 'byte',
   },
   POSITION: {
     FIXED: 'fixed',
