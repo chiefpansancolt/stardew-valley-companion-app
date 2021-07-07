db04b644cdbe65d9de88f0423c0250870f3bcba1
Christopher Pezza <chris.pezza@hey.com>
2021-07-07 16:15:49 -0400

    New Repo Structure with next.js base

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Site Title Here</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="Site Description Here" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div></div>
+    </div>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,44 @@
+const colors = require('tailwindcss/colors');
+
+module.exports = {
+  mode: 'jit',
+  purge: ['./pages/**/*.js', './components/**/*.js', './data/**/*.js'],
+  darkMode: false,
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+
+      black: '#000',
+      white: '#fff',
+
+      amber: colors.amber,
+      blue: colors.blue,
+      cyan: colors.cyan,
+      emerald: colors.emerald,
+      fuchsia: colors.fuchsia,
+      gray: colors.coolGray,
+      green: colors.green,
+      indigo: colors.indigo,
+      'light-blue': colors.lightBlue,
+      lime: colors.lime,
+      orange: colors.orange,
+      pink: colors.pink,
+      purple: colors.purple,
+      red: colors.red,
+      rose: colors.rose,
+      teal: colors.teal,
+      violet: colors.violet,
+      yellow: colors.yellow,
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/line-clamp'),
+  ],
+}
