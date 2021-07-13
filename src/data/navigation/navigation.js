91725e8819ab72836ae49498a5ad2e2d34e3a2f7
Christopher Pezza <chris.pezza@hey.com>
2021-07-12 21:15:07 -0400

    Add Food Page

--- src/data/navigation/navigation.js
+++ src/data/navigation/navigation.js
@@ -31,7 +31,7 @@ const navigation = [
       { name: "Minerals", href: "/collection/minerals", icon: faGem, standardIcon: false },
       { name: "Fish", href: "/collection/fish", icon: faFish, standardIcon: false },
       { name: "Artisan Goods", href: "/collection/artisan-goods", icon: faCheese, standardIcon: false },
-      { name: "Food", href: "#", icon: faHamburger, standardIcon: false },
+      { name: "Food", href: "/collection/food", icon: faHamburger, standardIcon: false },
       { name: "Foraging", href: "#", icon: faTree, standardIcon: false },
       { name: "Crops", href: "#", icon: faSeedling, standardIcon: false },
       { name: "Crafting", href: "#", icon: faHammer, standardIcon: false },
---
+++ src/pages/collection/food.js
@@ -0,0 +1,7 @@
+import Base from "@/components/page-headings/base";
+
+export default function Food() {
+  return(
+    <Base title="Food" showButtons={true} />
+  )
+}
