0d49eb17b73102c0e3c72324ec499b4b4f267dc2
Christopher Pezza <chris.pezza@hey.com>
2021-07-12 20:55:08 -0400

    Add Settings Page

--- src/data/navigation/secondary-navigation.js
+++ src/data/navigation/secondary-navigation.js
@@ -1,7 +1,7 @@
 import { CogIcon, QuestionMarkCircleIcon, ShieldCheckIcon } from "@heroicons/react/outline";
 
 const secondaryNavigation = [
-  { name: "Settings", href: "#", icon: CogIcon, standardIcon: true },
+  { name: "Settings", href: "/settings", icon: CogIcon, standardIcon: true },
   { name: "Help", href: "#", icon: QuestionMarkCircleIcon, standardIcon: true },
   { name: "Privacy", href: "#", icon: ShieldCheckIcon, standardIcon: true },
 ];
---
+++ src/pages/settings.js
@@ -0,0 +1,7 @@
+import Base from "@/components/page-headings/base";
+
+export default function Settings() {
+  return(
+    <Base title="Settings" showButtons={false} />
+  )
+}
