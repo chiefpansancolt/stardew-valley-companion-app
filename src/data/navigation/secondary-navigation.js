1701878be39bb5ad19e48931b1fed760171d2679
Christopher Pezza <chris.pezza@hey.com>
2021-07-12 20:55:55 -0400

    Add Privacy Page

--- src/data/navigation/secondary-navigation.js
+++ src/data/navigation/secondary-navigation.js
@@ -3,7 +3,7 @@ import { CogIcon, QuestionMarkCircleIcon, ShieldCheckIcon } from "@heroicons/rea
 const secondaryNavigation = [
   { name: "Settings", href: "/settings", icon: CogIcon, standardIcon: true },
   { name: "Help", href: "/help", icon: QuestionMarkCircleIcon, standardIcon: true },
-  { name: "Privacy", href: "#", icon: ShieldCheckIcon, standardIcon: true },
+  { name: "Privacy", href: "/privacy", icon: ShieldCheckIcon, standardIcon: true },
 ];
 
 export default secondaryNavigation;
---
+++ src/pages/privacy.js
@@ -0,0 +1,7 @@
+import Base from "@/components/page-headings/base";
+
+export default function Privacy() {
+  return(
+    <Base title="Privacy" showButtons={false} />
+  )
+}
