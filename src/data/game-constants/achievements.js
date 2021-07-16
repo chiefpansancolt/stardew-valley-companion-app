9d146efd6d483509fb8d191b62e6931e74bc73b8
Christopher Pezza <chris.pezza@hey.com>
2021-07-16 12:14:47 -0400

    Add initial structure for character page

--- src/data/game-constants/achievements.js
+++ src/data/game-constants/achievements.js
@@ -1,34 +1,279 @@
 const achievements = [
-  { key: 0, value: { name: "Greenhorn (15k)", description: "Earn 15,000g" } },
-  { key: 1, value: { name: "Cowpoke (50k)", description: "Earn 50,000g" } },
-  { key: 2, value: { name: "Homesteader (250k)", description: "Earn 250,000g" } },
-  { key: 3, value: { name: "Millionaire (1mil)", description: "Earn 1,000,000g" } },
-  { key: 4, value: { name: "Legend (10mil)", description: "Earn 10,000,000g" } },
-  { key: 5, value: { name: "A Complete Collection", description: "Complete the museum collection." } },
-  { key: 6, value: { name: "A New Friend", description: "Reach a 5-heart friend level with someone." } },
-  { key: 7, value: { name: "Best Friends", description: "Reach a 10-heart friend level with someone." } },
-  { key: 9, value: { name: "The Beloved Farmer", description: "Reach a 10-heart friend level with 8 people." } },
-  { key: 11, value: { name: "Cliques", description: "Reach a 5-heart friend level with 4 people." } },
-  { key: 12, value: { name: "Networking", description: "Reach a 5-heart friend level with 10 people." } },
-  { key: 13, value: { name: "Popular", description: "Reach a 5-heart friend level with 20 people." } },
-  { key: 15, value: { name: "Cook", description: "Cook 10 different recipes." } },
-  { key: 16, value: { name: "Sous Chef", description: "Cook 25 different recipes." } },
-  { key: 17, value: { name: "Gourmet Chef", description: "Cook every recipe." } },
-  { key: 18, value: { name: "Moving Up", description: "Upgrade your house." } },
-  { key: 19, value: { name: "Living Large", description: "Upgrade your house to the maximum size." } },
-  { key: 20, value: { name: "D.I.Y.", description: "Craft 15 different items." } },
-  { key: 21, value: { name: "Artisan", description: "Craft 30 different items." } },
-  { key: 22, value: { name: "Craft Master", description: "Craft every item." } },
-  { key: 24, value: { name: "Fisherman", description: "Catch 10 different fish." } },
-  { key: 25, value: { name: "Ol' Mariner", description: "Catch 24 different fish." } },
-  { key: 26, value: { name: "Master Angler", description: "Catch every fish." } },
-  { key: 27, value: { name: "Mother Catch", description: "Catch 100 fish." } },
-  { key: 28, value: { name: "Treasure Trove", description: "Donate 40 different items to the museum." } },
-  { key: 29, value: { name: "Gofer", description: "Complete 10 'Help Wanted' requests." } },
-  { key: 30, value: { name: "A Big Help", description: "Complete 40 'Help Wanted' requests." } },
-  { key: 31, value: { name: "Polyculture", description: "Ship 15 of each crop." } },
-  { key: 32, value: { name: "Monoculture", description: "Ship 300 of one crop." } },
-  { key: 34, value: { name: "Full Shipment", description: "Ship every item." } }
+  {
+    key: 0,
+    value: {
+      name: "Greenhorn (15k)",
+      description: "Earn 15,000g",
+      linkType: "Money",
+      detailLink: "",
+      amount: 15000,
+    },
+  },
+  {
+    key: 1,
+    value: {
+      name: "Cowpoke (50k)",
+      description: "Earn 50,000g",
+      linkType: "Money",
+      detailLink: "",
+      amount: 50000,
+    },
+  },
+  {
+    key: 2,
+    value: {
+      name: "Homesteader (250k)",
+      description: "Earn 250,000g",
+      linkType: "Money",
+      detailLink: "",
+      amount: 250000,
+    },
+  },
+  {
+    key: 3,
+    value: {
+      name: "Millionaire (1mil)",
+      description: "Earn 1,000,000g",
+      linkType: "Money",
+      detailLink: "",
+      amount: 1000000,
+    },
+  },
+  {
+    key: 4,
+    value: {
+      name: "Legend (10mil)",
+      description: "Earn 10,000,000g",
+      linkType: "Money",
+      detailLink: "",
+      amount: 10000000,
+    },
+  },
+  {
+    key: 5,
+    value: {
+      name: "A Complete Collection",
+      description: "Complete the museum collection.",
+      linkType: "Link",
+      detailLink: "/collection/artifacts",
+    },
+  },
+  {
+    key: 6,
+    value: {
+      name: "A New Friend",
+      description: "Reach a 5-heart friend level with someone.",
+      linkType: "Link",
+      detailLink: "/town-people",
+    },
+  },
+  {
+    key: 7,
+    value: {
+      name: "Best Friends",
+      description: "Reach a 10-heart friend level with someone.",
+      linkType: "Link",
+      detailLink: "/town-people",
+    },
+  },
+  {
+    key: 9,
+    value: {
+      name: "The Beloved Farmer",
+      description: "Reach a 10-heart friend level with 8 people.",
+      linkType: "Link",
+      detailLink: "/town-people",
+    },
+  },
+  {
+    key: 11,
+    value: {
+      name: "Cliques",
+      description: "Reach a 5-heart friend level with 4 people.",
+      linkType: "Link",
+      detailLink: "/town-people",
+    },
+  },
+  {
+    key: 12,
+    value: {
+      name: "Networking",
+      description: "Reach a 5-heart friend level with 10 people.",
+      linkType: "Link",
+      detailLink: "/town-people",
+    },
+  },
+  {
+    key: 13,
+    value: {
+      name: "Popular",
+      description: "Reach a 5-heart friend level with 20 people.",
+      linkType: "Link",
+      detailLink: "/town-people",
+    },
+  },
+  {
+    key: 15,
+    value: {
+      name: "Cook",
+      description: "Cook 10 different recipes.",
+      linkType: "Link",
+      detailLink: "/collection/food",
+    },
+  },
+  {
+    key: 16,
+    value: {
+      name: "Sous Chef",
+      description: "Cook 25 different recipes.",
+      linkType: "Link",
+      detailLink: "/collection/food",
+    },
+  },
+  {
+    key: 17,
+    value: {
+      name: "Gourmet Chef",
+      description: "Cook every recipe.",
+      linkType: "Link",
+      detailLink: "/collection/food",
+    },
+  },
+  {
+    key: 18,
+    value: {
+      name: "Moving Up",
+      description: "Upgrade your house.",
+      linkType: "House",
+      detailLink: "",
+    },
+  },
+  {
+    key: 19,
+    value: {
+      name: "Living Large",
+      description: "Upgrade your house to the maximum size.",
+      linkType: "House",
+      detailLink: "",
+    },
+  },
+  {
+    key: 20,
+    value: {
+      name: "D.I.Y.",
+      description: "Craft 15 different items.",
+      linkType: "Link",
+      detailLink: "/collection/crafting",
+    },
+  },
+  {
+    key: 21,
+    value: {
+      name: "Artisan",
+      description: "Craft 30 different items.",
+      linkType: "Link",
+      detailLink: "/collection/crafting",
+    },
+  },
+  {
+    key: 22,
+    value: {
+      name: "Craft Master",
+      description: "Craft every item.",
+      linkType: "Link",
+      detailLink: "/collection/crafting",
+    },
+  },
+  {
+    key: 24,
+    value: {
+      name: "Fisherman",
+      description: "Catch 10 different fish.",
+      linkType: "Link",
+      detailLink: "/collection/fish",
+    },
+  },
+  {
+    key: 25,
+    value: {
+      name: "Ol' Mariner",
+      description: "Catch 24 different fish.",
+      linkType: "Link",
+      detailLink: "/collection/fish",
+    },
+  },
+  {
+    key: 26,
+    value: {
+      name: "Master Angler",
+      description: "Catch every fish.",
+      linkType: "Link",
+      detailLink: "/collection/fish",
+    },
+  },
+  {
+    key: 27,
+    value: {
+      name: "Mother Catch",
+      description: "Catch 100 fish.",
+      linkType: "Link",
+      detailLink: "/collection/fish",
+    },
+  },
+  {
+    key: 28,
+    value: {
+      name: "Treasure Trove",
+      description: "Donate 40 different items to the museum.",
+      linkType: "Link",
+      detailLink: "/collection/artifacts",
+    },
+  },
+  {
+    key: 29,
+    value: {
+      name: "Gofer",
+      description: "Complete 10 'Help Wanted' requests.",
+      linkType: "Link",
+      detailLink: "/quests",
+    },
+  },
+  {
+    key: 30,
+    value: {
+      name: "A Big Help",
+      description: "Complete 40 'Help Wanted' requests.",
+      linkType: "Link",
+      detailLink: "/quests",
+    },
+  },
+  {
+    key: 31,
+    value: {
+      name: "Polyculture",
+      description: "Ship 15 of each crop.",
+      linkType: "Link",
+      detailLink: "/collection/crops",
+    },
+  },
+  {
+    key: 32,
+    value: {
+      name: "Monoculture",
+      description: "Ship 300 of one crop.",
+      linkType: "Link",
+      detailLink: "/collection/crops",
+    },
+  },
+  {
+    key: 34,
+    value: {
+      name: "Full Shipment",
+      description: "Ship every item.",
+      linkType: "Link",
+      detailLink: "/shipment",
+    },
+  },
 ];
 
 export default achievements;
--- src/data/game-constants/skills.js
+++ src/data/game-constants/skills.js
@@ -1,6 +1,6 @@
-const skillLevelNumbers = [100,380,770,1300,2150,3300,4800,6900,10000,15000];
+const skillLevelNumbers = [100, 380, 770, 1300, 2150, 3300, 4800, 6900, 10000, 15000];
 
-const skillTitle = function(skillTotal, isMale) {
+const skillTitle = function (skillTotal, isMale) {
   switch (skillTotal) {
     case 0:
     case 1:
@@ -50,6 +50,226 @@ const skillTitle = function(skillTotal, isMale) {
     default:
       return "Farm King";
   }
-}
+};
 
-export { skillTitle, skillLevelNumbers } ;
+const skills = [
+  {
+    name: "Farming",
+    img: "https://stardewvalleywiki.com/mediawiki/images/8/82/Farming_Skill_Icon.png",
+    description:
+      "Farming skill is gained by harvesting crops. Caring for animals also adds experience: Petting, milking, and shearing farm animals or picking up an animal product inside a barn or coop each give 5 experience points. Using a hoe or watering can does not grant experience by itself. Each level grants +1 proficiency to hoes and watering cans.",
+    height: "h-44",
+    levels: [
+      {
+        name: "Level 1",
+        num: 1,
+        items: [
+          {
+            name: "Scarecrow",
+            img: "https://stardewvalleywiki.com/mediawiki/images/thumb/7/75/Scarecrow.png/24px-Scarecrow.png",
+            link: "/collection/crafting",
+          },
+          {
+            name: "Basic Fertilizer",
+            img: "https://stardewvalleywiki.com/mediawiki/images/thumb/9/9b/Basic_Fertilizer.png/24px-Basic_Fertilizer.png",
+            link: "/collection/crafting",
+          },
+        ],
+      },
+      {
+        name: "Level 2",
+        num: 2,
+        items: [
+          {
+            name: "Mayonnaise Machine",
+            img: "https://stardewvalleywiki.com/mediawiki/images/thumb/e/ef/Mayonnaise_Machine.png/24px-Mayonnaise_Machine.png",
+            link: "/collection/crafting",
+          },
+          {
+            name: "Stone Fence",
+            img: "https://stardewvalleywiki.com/mediawiki/images/thumb/2/2d/Stone_Fence.png/24px-Stone_Fence.png",
+            link: "/collection/crafting",
+          },
+          {
+            name: "Sprinkler",
+            img: "https://stardewvalleywiki.com/mediawiki/images/thumb/0/08/Sprinkler.png/24px-Sprinkler.png",
+            link: "/collection/crafting",
+          },
+        ],
+      },
+      {
+        name: "Level 3",
+        num: 3,
+        items: [
+          {
+            name: "Bee House",
+            img: "https://stardewvalleywiki.com/mediawiki/images/thumb/c/ce/Bee_House.png/24px-Bee_House.png",
+            link: "/collection/crafting",
+          },
+          {
+            name: "Speed-Gro",
+            img: "https://stardewvalleywiki.com/mediawiki/images/thumb/9/94/Speed-Gro.png/24px-Speed-Gro.png",
+            link: "/collection/crafting",
+          },
+          {
+            name: "Farmer's Lunch",
+            img: "https://stardewvalleywiki.com/mediawiki/images/7/79/Farmer%27s_Lunch.png",
+            link: "/collection/food",
+          },
+        ],
+      },
+      {
+        name: "Level 4",
+        num: 4,
+        items: [
+          {
+            name: "Preserves Jar",
+            img: "https://stardewvalleywiki.com/mediawiki/images/thumb/1/1e/Preserves_Jar.png/24px-Preserves_Jar.png",
+            link: "/collection/crafting",
+          },
+          {
+            name: "Basic Retaining Soil",
+            img: "https://stardewvalleywiki.com/mediawiki/images/thumb/c/c7/Basic_Retaining_Soil.png/24px-Basic_Retaining_Soil.png",
+            link: "/collection/crafting",
+          },
+          {
+            name: "Iron Fence",
+            img: "https://stardewvalleywiki.com/mediawiki/images/thumb/9/9d/Iron_Fence.png/24px-Iron_Fence.png",
+            link: "/collection/crafting",
+          },
+        ],
+      },
+      {
+        name: "Level 5",
+        num: 5,
+        items: [
+          [
+            {
+              name: "Rancher",
+              img: "https://stardewvalleywiki.com/mediawiki/images/thumb/3/3c/Rancher.png/24px-Rancher.png",
+              description: "Animal products worth 20% more.",
+            },
+          ],
+          [
+            {
+              name: "Tiller",
+              img: "https://stardewvalleywiki.com/mediawiki/images/thumb/b/b2/Tiller.png/24px-Tiller.png",
+              description: "Crops worth 10% more.",
+            },
+          ],
+        ],
+      },
+      {
+        name: "Level 6",
+        num: 6,
+        items: [
+          {
+            name: "Cheese Press",
+            img: "https://stardewvalleywiki.com/mediawiki/images/thumb/7/79/Cheese_Press.png/24px-Cheese_Press.png",
+            link: "/collection/crafting",
+          },
+          {
+            name: "Hardwood Fence",
+            img: "https://stardewvalleywiki.com/mediawiki/images/thumb/5/5c/Hardwood_Fence.png/24px-Hardwood_Fence.png",
+            link: "/collection/crafting",
+          },
+          {
+            name: "Quality Sprinkler",
+            img: "https://stardewvalleywiki.com/mediawiki/images/thumb/a/af/Quality_Sprinkler.png/24px-Quality_Sprinkler.png",
+            link: "/collection/crafting",
+          },
+        ],
+      },
+      {
+        name: "Level 7",
+        num: 7,
+        items: [
+          {
+            name: "Loom",
+            img: "https://stardewvalleywiki.com/mediawiki/images/thumb/3/3b/Loom.png/24px-Loom.png",
+            link: "/collection/crafting",
+          },
+          {
+            name: "Quality Retaining Soil",
+            img: "https://stardewvalleywiki.com/mediawiki/images/thumb/0/0a/Quality_Retaining_Soil.png/24px-Quality_Retaining_Soil.png",
+            link: "/collection/crafting",
+          },
+        ],
+      },
+      {
+        name: "Level 8",
+        num: 8,
+        items: [
+          {
+            name: "Oil Maker",
+            img: "https://stardewvalleywiki.com/mediawiki/images/thumb/c/c5/Oil_Maker.png/24px-Oil_Maker.png",
+            link: "/collection/crafting",
+          },
+          {
+            name: "Keg",
+            img: "https://stardewvalleywiki.com/mediawiki/images/thumb/7/7c/Keg.png/24px-Keg.png",
+            link: "/collection/crafting",
+          },
+          {
+            name: "Deluxe Speed-Gro",
+            img: "https://stardewvalleywiki.com/mediawiki/images/thumb/6/6d/Deluxe_Speed-Gro.png/24px-Deluxe_Speed-Gro.png",
+            link: "/collection/crafting",
+          },
+        ],
+      },
+      {
+        name: "Level 9",
+        num: 9,
+        items: [
+          {
+            name: "Seed Maker",
+            img: "https://stardewvalleywiki.com/mediawiki/images/thumb/1/19/Seed_Maker.png/24px-Seed_Maker.png",
+            link: "/collection/crafting",
+          },
+          {
+            name: "Iridium Sprinkler",
+            img: "https://stardewvalleywiki.com/mediawiki/images/thumb/9/90/Iridium_Sprinkler.png/24px-Iridium_Sprinkler.png",
+            link: "/collection/crafting",
+          },
+          {
+            name: "Quality Fertilizer",
+            img: "https://stardewvalleywiki.com/mediawiki/images/thumb/a/a0/Quality_Fertilizer.png/24px-Quality_Fertilizer.png",
+            link: "/collection/crafting",
+          },
+        ],
+      },
+      {
+        name: "Level 10",
+        num: 10,
+        items: [
+          [
+            {
+              name: "Coopmaster",
+              img: "https://stardewvalleywiki.com/mediawiki/images/thumb/2/26/Coopmaster.png/24px-Coopmaster.png",
+              description: "Befriend coop animals quicker. Incubation time cut in half.",
+            },
+            {
+              name: "Shepherd",
+              img: "https://stardewvalleywiki.com/mediawiki/images/thumb/8/80/Shepherd.png/24px-Shepherd.png",
+              description: "Befriend barn animals quicker. Sheep produce wool faster.",
+            },
+          ],
+          [
+            {
+              name: "Artisan",
+              img: "https://stardewvalleywiki.com/mediawiki/images/thumb/4/46/Artisan.png/24px-Artisan.png",
+              description: "Artisan goods (wine, cheese, oil, etc.) worth 40% more.",
+            },
+            {
+              name: "Agriculturist",
+              img: "https://stardewvalleywiki.com/mediawiki/images/thumb/7/78/Agriculturist.png/24px-Agriculturist.png",
+              description: "All crops grow 10% faster.",
+            },
+          ],
+        ],
+      },
+    ],
+  },
+];
+
+export { skillTitle, skillLevelNumbers, skills };
